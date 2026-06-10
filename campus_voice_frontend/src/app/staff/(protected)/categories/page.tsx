b7cc5e0645b4af2bfc84cd4f883ac5695bbba9d5
Soponloe <sovannsoponloe@gmail.com>
2026-06-10 23:00:35 +0700

    modular

---
+++ campus_voice_frontend/src/app/staff/(protected)/categories/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { CategoryManagementPanel } from "@/components/categories/CategoryManagementPanel";
+import { RoleDashboardShell } from "@/components/layout/RoleDashboardShell";
+import { staffNav } from "@/lib/staff-nav";
+
+export default function StaffCategoriesPage() {
+  return (
+    <RoleDashboardShell
+      roleName="Staff"
+      title="Categories"
+      description="Manage report categories, default priorities, and availability for student submissions."
+      navItems={staffNav}
+    >
+      <CategoryManagementPanel />
+    </RoleDashboardShell>
+  );
+}
