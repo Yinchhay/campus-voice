1c84c93e748358d29be1d6367dbb9a6e407967fe
Yinchhay <134267753+Yinchhay@users.noreply.github.com>
2026-06-15 16:47:14 +0700

    fix: add default from email

--- campus_voice_backend/config/settings.py
+++ campus_voice_backend/config/settings.py
@@ -178,6 +178,7 @@ CSRF_TRUSTED_ORIGINS = config(
 )
 
 # EMAIL CONFIGURATION
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 
 BREVO_API_KEY = config('BREVO_API_KEY', default='')
 if BREVO_API_KEY:
@@ -191,6 +192,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Campus Voice <noreply@campus-voice.xyz>')
 
 # Logging Configuration
 LOGGING = {
