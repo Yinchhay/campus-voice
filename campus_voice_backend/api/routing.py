09e2ee0a7098a25dc7c0c5a872b22fc203b31c00
Yinchhay <134267753+Yinchhay@users.noreply.github.com>
2026-06-25 01:07:14 +0700

    fix: add daphne to docker compose changed from Gunicorn

--- campus_voice_backend/api/routing.py
+++ campus_voice_backend/api/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from api.consumers import TicketChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/tickets/(?P<ticket_id>[^/]+)/$', TicketChatConsumer.as_asgi()),
+    re_path(r'^ws/tickets/(?P<ticket_id>[^/]+)/$', TicketChatConsumer.as_asgi()),
 ]
